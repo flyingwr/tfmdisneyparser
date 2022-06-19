f55d57ffa2163250da64ef780be0ea104c6495bf
renan fernandes <leekchuucmrc@gmail.com>
2022-06-19 10:47:26 -0300

    update player.py

--- tfmparser/player.py
+++ tfmparser/player.py
@@ -136,4 +136,10 @@ class Player(dict):
 						self["player_id"] = getproperty.group(2)
 						break
 
+		for line, content in enumerate(dumpscript):
+			if "getlocal r5" in content and "getlocal_1" in dumpscript[line + 1] and "getproperty" in dumpscript[line + 2]:
+				if (setproperty := await find_one(SET_PROPERTY, dumpscript[line + 3])) is not None:
+					if "getlocal r5" in dumpscript[line + 4] and "iffalse" in dumpscript[line + 5]:
+						print("ok")
+
 		return self
